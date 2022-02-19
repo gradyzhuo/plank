9a0b36c871b491cc9f811c96ae7ea149b1283bac
Grady Zhuo <gradyzhuo@gmail.com>
2022-02-19 16:02:15 +0800

    @add context

--- polymath/app/__init__.py
+++ polymath/app/__init__.py
@@ -1,12 +1,13 @@
 from __future__ import annotations
-from polymath.plugins import Plugin
-from polymath.plugins.manager import PluginManager
-from polymath.plugins.resource import Resource
-from polymath.serving.extension import Extension, ExtensionInfo
-from typing import Optional, List, Any, Generator, Union, Dict, Type
 from pathlib import Path
-from functools import reduce
+from typing import Optional, List, Any, Generator, Union, Dict, Type
+from polymath import logger
+from polymath.plugin import Plugin
+from polymath.plugin.module import ModulePlugin
 from polymath.config import Configuration, flatten_dict
+from polymath.serving.service import Service
+from polymath.server import Server
+from polymath.app.context import Context
 
 _Application__singleton_key = "__sigleton"
 
@@ -15,8 +16,22 @@ class ApplicationDelegate:
         def application_did_launch(self, app:Application): pass
         def application_discover_plugin_prefix(self, app: Application)->str:
             return f"{app.name}_plugin_"
+        def application_using_plugin_type(self, app: Application) -> Type[Plugin]:
+            return ModulePlugin
+
         def application_did_discover_plugins(self, app: Application, plugins:List[Plugin]): pass
+        def application_should_install_plugin(self, app: Application, plugin: Plugin) -> bool: return True
+        def application_did_install_plugin(self, app: Application, plugin: Plugin): pass
         def application_should_load_plugin(self, app: Application, plugin:Plugin)->bool: return True
+        def application_did_load_plugin(self, app: Application, plugin: Plugin): pass
+
+
+class ServingManager:
+    def __init__(self):
+        pass
+
+    def set(self):
+        pass
 
 
 class Application:
@@ -34,21 +49,21 @@ class Application:
     def version(self)->str:
         return self.__version
 
-    # @property
-    # def mode(self)->str:
-    #     return self.configuration.mode_name
-
     @property
     def workspace(self)->Path:
-        return self.__workspace
+        return self.configuration.path.workspace
 
-    # @property
-    # def debug_mode(self)->bool:
-    #     return self.configuration.debug_mode
-    #
-    # @property
-    # def configuration(self)->Configuration:
-    #     return self.__configuration
+    @property
+    def debug(self)->bool:
+        return self.configuration.app.debug
+
+    @property
+    def plugin_name_prefix(self)->str:
+        return self.__plugin_name_prefix
+
+    @property
+    def configuration(self)->Configuration:
+        return self.__configuration
 
     @property
     def delegate(self)->ApplicationDelegate:
@@ -56,99 +71,129 @@ class Application:
 
     @property
     def plugins(self)->List[Plugin]:
-        return self.__plugin_manager.plugins
+        plugin_type = self.delegate.application_using_plugin_type(app=self)
+        return plugin_type.installed()
 
     @property
     def loaded(self):
         return self.__loaded
 
     @property
-    def launch_options(self)->Dict[str, str]:
-        return self.__launch_options or {}
+    def server(self)->Optional[Server]:
+        return self.__server
 
     @classmethod
     def main(cls)->Application:
         if not hasattr(cls, _Application__singleton_key):
             raise RuntimeError(f"Application had no an instance. please get instance by Application(delegate=...) first.")
         return getattr(cls, _Application__singleton_key)
+
+    @classmethod
+    def construct(cls, name:str, version: str, delegate_type: Type[ApplicationDelegate], workspace_path: Path, **kwargs)->Application:
+        defaults = Context.standard()
+        defaults.set("workspace_path", str(workspace_path))
+        defaults.set("application.name", name)
+        defaults.set("app.name", name)
+
+        configuration_path = workspace_path / "configuration" / "configuration.toml"
+        Configuration.preload(path=configuration_path)
+        delegate = delegate_type()
+        application = Application(name=name, version=version, delegate=delegate, **kwargs)
+        if not hasattr(cls, _Application__singleton_key):
+            application.as_main()
+        return application
+
     
-    def __init__(self, name: str, version: str, delegate: ApplicationDelegate) -> None:
+    def __init__(self, name:str, version: str, delegate: ApplicationDelegate, **kwargs) -> None:
         self.__name = name
         self.__version = version
-        self.__plugin_manager: PluginManager = PluginManager.default()
         self.__delegate = delegate
         self.__loaded = False
         self.__launch_options = None
-
-        # self.__configuration = configuration
-
-        if not hasattr(Application, _Application__singleton_key):
-            self.as_main()
+        self.__configuration: Configuration = None
+        self.__server = None
+        self.__plugin_name_prefix = delegate.application_discover_plugin_prefix(app=self)
 
 
     def as_main(self):
         setattr(Application, _Application__singleton_key, self)
-        
-
-    def find_extensions(self, name:Optional[str]=None, extension_type: Optional[Type[Extension]]=None)->Generator[ExtensionInfo, None, None]:
-        extension_infos = reduce(lambda results, plugin: results + plugin.extensions(type=extension_type), self.__plugin_manager.plugins, [])
-        return filter(lambda extension_info: (extension_info.name == name) ^ (name is None), extension_infos)
-
-    def extension_info(self, name: str, by_type: Type[Extension]) -> Optional[ExtensionInfo]:
-        extension_infos = self.find_extensions(name=name, extension_type=by_type)
-        try:
-            return next(extension_infos)
-        except:
-            return None
-
-    def extension(self, name:str, by_type: Type[Extension])->Optional[Extension]:
-        extension_info = self.extension_info(name=name, by_type=by_type)
-        return extension_info.extension if extension_info is not None else None
 
     def plugin(self, name:str)->Plugin:
-        return self.__plugin_manager.get_plugin(name)
+        plugin_type = self.delegate.application_using_plugin_type(app=self)
+        return plugin_type.plugin(name=name)
 
-    def resource(self, resource_name: str, in_plugin: Union[str, Plugin]) -> Optional[Resource]:
-        in_plugin = in_plugin if isinstance(in_plugin, Plugin) else self.plugin(in_plugin)
-        if in_plugin is not None:
-            return in_plugin.shared_resources.get(resource_name)
+    def add_service(self, service: Service, name: Optional[str]=None):
+        Service.register(service=service, name=name)
 
-    def load(self, workspace: Path, configuration_path: Path, launch_options: Optional[Dict[str, Any]]=None):
-        if self.__loaded:
-            return
+    def add_services_by_plugin(self, plugin: Plugin):
+        for service in plugin.services:
+            Service.register(service=service, name=f"{plugin.name}.{service.name()}", plugin=plugin.name)
 
-        # config_name = config_name or "debug"
+    def _load_plugin(self):
+        plugin_type = self.delegate.application_using_plugin_type(app=self)
+        plugins = plugin_type.discover(plugin_prefix=self.__plugin_name_prefix)
 
-        # configuration_path = workspace / "configuration" / f"{config_name}.yml"
-        configuration = Configuration.from_file(file_path=configuration_path)
-        configuration.add_path("workspace", workspace)
-        configuration.set_default()
-
-        launch_options = launch_options or self.launch_options
-        configuration.reload(extra=launch_options)
-        self.__loaded = True
-
-        plugin_prefix = self.delegate.application_discover_plugin_prefix(app=self)
-        plugins = self.__plugin_manager.discover(plugin_prefix=plugin_prefix, install=True)
         if len(plugins) > 0:
             self.delegate.application_did_discover_plugins(app=self, plugins=plugins)
+        for plugin in plugins:
+            try:
+                if self.delegate.application_should_install_plugin(app=self, plugin=plugin):
+                    plugin_type.install(plugin=plugin)
+                    self.delegate.application_did_install_plugin(app=self, plugin=plugin)
+                if self.delegate.application_should_load_plugin(app=self, plugin=plugin):
+                    plugin.load()
+                    self.delegate.application_did_load_plugin(app=self, plugin=plugin)
+            except Exception as e:
+                logger.error(f"The error happened on loading plugin: {plugin.name}.")
+                raise e
 
-        for plugin in self.plugins:
-            if self.delegate.application_should_load_plugin(app=self, plugin=plugin):
-                plugin.load()
+            self.add_services_by_plugin(plugin=plugin)
+
+                # server = InlineServer.by_host(host=plugin.name.lower(), application=self)
+                # plugin.launch(server=server, **launch_options)
+        # from polymath.serving.connector import Connector
+        # for category in self.configuration.extension.categories:
+        #     for extension_name in self.configuration.extension.names_for_category(category=category):
+        #         connector_dict = self.configuration.extension.connector_dict(name=extension_name, category=category)
+        #         connector_url = connector_dict["url"]
+        #         connector_type = Connector.get_type(url=connector_url)
+        #         connector = connector_type(url=connector_url)
+        #         proxy = Extension.proxy(connector=connector)
+        #         extension_info = ExtensionInfo(name=extension_name, instance=proxy)
+        #         #未完成
+
+
+    def load(self, program:str, launch_options: Optional[Dict[str, Any]]=None):
+        if self.__loaded:
+            return
+        launch_options = launch_options or {}
+        standard_defaults = Context.standard()
+        standard_defaults.update(launch_options)
+        self.__loaded = True
+
 
     def unload(self):
         for plugin in self.plugins:
             plugin.unload()
         self.__loaded = False
 
-    def launch(self, workspace: Path, configuration_path: Path, **options):
+    def launch(self, program:str, **options):
+        assert program is not None, "The program of configuration is needed."
         self.__launch_options = options
+        self.__configuration = Configuration.from_program(program_name=program, as_default=True)
         self.__delegate.application_will_launch(self, options)
-        self.load(workspace=workspace, configuration_path=configuration_path,launch_options=options)
+        self.load(program=program, launch_options=options)
         self.__delegate.application_did_launch(self)
 
-    def did_set_launcher(self, launcher):
-        pass
+        self._load_plugin()
+        for plugin in self.plugins:
+            plugin.delegate.application_did_launch(plugin=plugin, launch_options=options)
 
+    def _server_did_startup(self, server: Server):
+        self.__server = server
+        for service in Service.registered():
+            server.add_backends_by_service(service)
+
+    def _server_did_shutdown(self, server: Server):
+        self.__server = None
 
---
+++ polymath/app/context.py
@@ -0,0 +1,98 @@
+from __future__ import annotations
+import threading, re, copy, os
+from copy import copy
+from typing import List, Tuple, Any, Optional, Union, Iterable, Dict
+
+_UserDefaults__singleton_key = "__Userdefault"
+
+class Context:
+
+    @classmethod
+    def standard(cls, namespace: Optional[str]=None) -> Context:
+        namespace = namespace or "main"
+        key = f"{_UserDefaults__singleton_key}.{namespace}"
+        lock = threading.Lock()
+        if not hasattr(cls, key):
+            with lock:
+                obj = cls(namespace=namespace)
+                setattr(cls, key, obj)
+        return getattr(cls, key)
+
+    @classmethod
+    def namespaces(cls, prefix: Optional[str]=None)->List[str]:
+        prefix = f"{_UserDefaults__singleton_key}_{prefix}" if prefix is not None else _UserDefaults__singleton_key
+        filtered_singleton_keys  = filter(lambda key: key.startswith(prefix), cls.__dict__.keys())
+        return [ key.replace(f"{_UserDefaults__singleton_key}.", "") for key in filtered_singleton_keys ]
+
+    @property
+    def namespace(self)->str:
+        return self.__namespace
+
+    def __init__(self, namespace: str):
+        self.__namespace = namespace
+        self.__contexts:Dict[str, Any] = {}
+        self.__rx = r'\$\{(?P<VAR>[a-z A-Z]{1}[._\w]+)\}'
+
+    def keys(self)->List[str]:
+        return list(self.__contexts.keys())
+
+    def values(self)->List[Any]:
+        return list(self.__contexts.values())
+
+    def items(self)->List[Tuple[str, Any]]:
+        return [(key, value) for key, value in self.__contexts.items()]
+
+    def dict(self)->Dict[str, Any]:
+        return copy(self.__contexts)
+
+    def set(self, key: str, value: Any):
+        self.__contexts[key] = value
+
+    def get(self, key: str, default: Optional[Any]=None, reword: Optional[bool]=None)-> Optional[Any]:
+        value = self.__contexts.get(key, default)
+        reword = reword if reword is not None else True
+        return self.reword(value) if reword else value
+
+    def remove(self, key: str):
+        del self.__contexts[key]
+
+    def update(self, _m: Union[Dict, Iterable], **kwargs):
+        self.__contexts.update(_m, **kwargs)
+
+    def __replace_var(self, string, replaced=lambda var_name: var_name):
+        def replaces(matchobj):
+            variable_key = matchobj.groupdict()["VAR"]
+            return replaced(variable_key)
+        return re.sub(self.__rx, replaces, str(string))
+
+    def _reword(self, var: str, extra_info:Dict[str, Any]=None)->Any:
+        grounds_dict = dict([item for item in self.items() if isinstance(item[1], (str, int, float, bool)) ])
+        extra_info = extra_info or {}
+        grounds_dict.update(os.environ)
+        grounds_dict.update(extra_info)
+        if self != Context.standard():
+            grounds_dict.update(Context.standard().__contexts)
+        result = grounds_dict.get(var) \
+                 or self.__replace_var(var, replaced=lambda var_name: grounds_dict.get(var_name, f"${{{var_name}}}"))
+        return result
+
+    def reword(self, var: str, extra_info:Dict[str, Any]=None)->Any:
+        if not isinstance(var, str):
+            return var
+        result = var
+        match_object = self.can_reword(result)
+        previous_var_name = None
+        while match_object is not None:
+            var_name = match_object["VAR"]
+            if previous_var_name != var_name:
+                previous_var_name = var_name
+            else:
+                break
+            result = self._reword(result, extra_info)
+            match_object = self.can_reword(result)
+        return result
+
+    def can_reword(self, var: str)->Optional[re.Match]:
+        if type(var) is not str:
+            return None
+        return re.search(self.__rx, var)
